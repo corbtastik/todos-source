f07c6c9ad06f7f7509e1eed8f00c28e6380cb7bf
Corbett Martin <corbtastik@gmail.com>
2019-02-06 13:18:57 -0600

    updated spring cloud and readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# todos-source
+# Todos Spring Cloud Stream example
+
+Howdy!  This is a small [Spring Cloud Streams](https://cloud.spring.io/spring-cloud-stream/) example that sources Todo events when one HTTP endpoint is POST'ed to.  The request is handled and in-turn fires an event which is processed by other microservices (such as [todos-processor](https://github.com/corbtastik/todos-processor) and [todos-sink](https://github.com/corbtastik/todos-sink)).
+
+## Use
+
+### Clone 3 Stream apps
+
+* [Todos Source](https://github.com/corbtastik/todos-source) - event Todos
+* [Todos Processor](https://github.com/corbtastik/todos-processor) - process Todos
+* [Todos Sink](https://github.com/corbtastik/todos-sink) - handle Todos
+
+### Build each app
+
+``mvnw clean package``
+
+### Start each app
+
+```bash
+java -jar ./target/todos-source-1.0.0-SNAP.jar --server.port=8080
+java -jar ./target/todos-processor-1.0.0-SNAP.jar --server.port=8081
+java -jar ./target/todos-sink-1.0.0-SNAP.jar --server.port=8082
+```
+
+### Invoke Stream
+
+POST a todo to the todos-source endpoint, for example using [HttpPie](https://httpie.org/) to make the call...
+
+```bash
+http :8080/ title="#one #two three"
+```
+
+### Observer
+
+* [Todos Processor](https://github.com/corbtastik/todos-processor) - If the Todo contains #hashtags a statement is logged and the #hashtags are parsed into a set containing #hashtags.
+* [Todos Sink](https://github.com/corbtastik/todos-sink) - Logs todo events
+
+
+
--- src/main/java/io/corbs/TodosSourceApp.java
+++ src/main/java/io/corbs/TodosSourceApp.java
@@ -1,30 +1,49 @@
 package io.corbs;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.http.MediaType;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.GenericMessage;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+class Todo {
+    private Integer id;
+    private String title = "";
+    private Boolean completed = false;
+}
 
 @SpringBootApplication
 @RestController
 @EnableBinding(TodosSourceApp.SourceChannels.class)
 public class TodosSourceApp {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TodosSourceApp.class);
+
+    private final static AtomicInteger seq = new AtomicInteger(1);
+
     interface SourceChannels {
         @Output
         MessageChannel output();
     }
 
-    private final SourceChannels channels;
+    private SourceChannels channels;
 
     @Autowired
     public TodosSourceApp(SourceChannels channels) {
@@ -32,19 +51,13 @@ public class TodosSourceApp {
     }
 
     @PostMapping("/")
-    public Todo createTodo(@RequestBody Todo todo) {
+    public Todo eventTodo(@RequestBody Todo todo) {
+        if(todo.getId() == null) {
+            todo.setId(seq.getAndIncrement());
+        }
         this.channels.output().send(new GenericMessage<>(todo));
-        // TODO return Todo value after its been inserted
+        LOG.info("Firing event for " + todo.toString());
         return todo;
-    }
-
-    @GetMapping(value = "/stats", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-    private Flux<Long> live() {
-        return Flux.create(fluxSink -> {
-            while(true) {
-                fluxSink.next(System.currentTimeMillis());
-            }
-        });
     }
 
     public static void main(String[] args) {
